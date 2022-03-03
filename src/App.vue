7e53a2522dbfe43b4ec898d452982a71ac2850bc
Carlos Martinez <curlosmart@gmail.com>
2022-03-03 15:02:45 -0500

    Add job listings

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,83 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div class="container">
+    <img src="/assets/images/bg-header-desktop.svg" alt="" class="bgImage"/>
+
+    <div class="filters">
+      <div class="filter">
+        <div>CSS</div>
+        <img src="/images/icon-remove.svg" alt="" />
+      </div>
+    </div>
+
+    <div class="jobs">
+      <div :key="job.id" v-for="job in jobs">
+        <JobListing :job="job"/>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import JobListing from './components/JobListing'
+import { getData } from './utils/getData.js'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    JobListing
+  },
+  data() {
+    return {
+      jobs: getData()
+    }
+  },
+  mounted() {
+    console.log(this.jobs)
   }
 }
 </script>
 
 <style>
+  @import url('https://fonts.googleapis.com/css2?family=Spartan:wght@500;700&display=swap');
 
+  * {
+    box-sizing: border-box;
+    margin: 0;
+  }
+
+  .container {
+    background-color: hsl(180, 52%, 96%);
+    min-height: 100vh;
+    margin: 0;
+    width: 100vw;
+    max-width: 100%;
+    font-family: Spartan, sans-serif;
+  }
+
+  .bgImage {
+    background-color: hsl(180, 29%, 50%);
+    object-fit: cover;
+    width: 100%;
+  }
+
+  .jobs {
+    margin-top: 30px;
+    padding: 40px;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+  }
+
+  .filters {
+    padding: 40px;
+    margin: 0px 40px;
+    background-color: #FFFFFF;
+    border-radius: 10px;
+  }
+
+  .filter {
+    background-color: hsl(180, 52%, 96%);
+    color: hsl(180, 29%, 50%);
+  }
 </style>
---
+++ src/components/JobListing.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="wrapper">
+    <div class="left">
+      <img :src="job.logo.slice(1,)" alt="job.company" />
+      <div class="leftDetails">
+        <div class="companyAndDate">
+          <div class="company">{{ job.company }}</div>
+          <button v-if="job.new" class="new">NEW!</button>
+          <button v-if="job.featured" class="featured">FEATURED</button>
+        </div>
+        <div class="position">{{ job.position }}</div>
+        <div class="info">
+          <span>{{ job.postedAt }}</span>
+          <span class="circle"></span>
+          <span>{{ job.contract }}</span>
+          <span class="circle"/>
+          <span>{{ job.location }}</span>
+        </div>
+      </div>
+      
+    </div>
+
+    <div class="right">
+      <div :key="tool" v-for="tool in job.tools" class="tool">
+        {{ tool }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'JobListing',
+  props: {
+    job: Object
+  }
+}
+</script>
+
+<style scoped>
+  .wrapper {
+    border-radius: 10px;
+    background-color: #FFFFFF;
+    padding: 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    box-shadow: 0px 6px 16px hsl(180, 29%, 50%);
+  }
+
+  .left {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+  }
+
+  .leftDetails {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+
+  .right {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    gap: 10px;
+  }
+
+  .tool {
+    background-color: hsl(180, 52%, 96%);
+    color: #5CA5A5;
+    padding: 10px;
+    border-radius: 10px;
+    font-size: 13px;
+    letter-spacing: -0.1px;
+    font-weight: bold;
+  }
+
+  .position {
+    font-weight: bold;
+    font-size: 18px;
+    color: #2B3939;
+  }
+
+  .companyAndDate {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+  }
+
+  .companyAndDate button {
+    border-radius: 12px;
+    padding: 10px;
+    font-size: 11px;
+    letter-spacing: -0.08px;
+    font-weight: bold;
+    color: #FFFFFF;
+    border: none;
+  }
+
+  .company {
+    font-size: 14px;
+    font-weight: bold;
+    color: #5CA5A5;
+  }
+
+  .new {
+    background-color: #5CA5A5;
+  }
+
+  .featured {
+    background-color: #2B3939;
+  }
+
+  .info span {
+    color: #7C8F8F;
+    font-size: 15px;
+    letter-spacing: -0.12px;
+    margin-right: 10px;
+  }
+
+  .circle {
+    height: 4px;
+    width: 4px;
+    color: red;
+    border-radius: 50%;
+  }
+</style>
